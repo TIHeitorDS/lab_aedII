0b5dca48bfdcbb4296804f4fa954eb4cc1ac6270
TIHeitorDS <heitorclaudinods@gmail.com>
2023-08-13 15:19:41 -0300

    correção da questão

--- primeira_unidade/7a_questao/main.c
+++ primeira_unidade/7a_questao/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int funcaoMatematica(int m, int n)
 {
@@ -10,8 +11,12 @@ int funcaoMatematica(int m, int n)
     {
         return n + 1;
     }
-
-    return funcaoMatematica(m, n - 1) + funcaoMatematica(m - 1, n);
+    if (m > 1 && n > 1)
+    {
+        return funcaoMatematica(m, n - 1) + funcaoMatematica(m - 1, n);
+    }
+    printf("Entrada invalida!\n");
+    exit(1);
 }
 
 int main()
@@ -21,6 +26,6 @@ int main()
     scanf("%d", &n);
     printf("informe outro valor: ");
     scanf("%d", &m);
-    printf("resultado: %d", funcaoMatematica(m, n   ));
+    printf("resultado: %d", funcaoMatematica(m, n));
     return 0;
 }
