a2729ee16603e2f5e6f9b3a919ac45c6b30dace7
TIHeitorDS <heitorclaudinods@gmail.com>
2023-08-14 11:14:58 -0300

    correção da q 2

--- primeira_unidade/2a_questao/main.c
+++ primeira_unidade/2a_questao/main.c
@@ -1,160 +1,25 @@
 #include <stdio.h>
 
-typedef struct diasHoje
+int diasDeAula(int dias, int mes, int ano)
 {
-    int dia;
-    int mes;
-    int ano;
-} DiasHoje;
-
-typedef struct fimSemestre
-{
-    int dia;
-    int mes;
-    int ano;
-} FimSemestre;
-
-typedef enum meses
-{
-    zero,
-    janeiro,
-    fevereiro,
-    marco,
-    abril,
-    maio,
-    junho,
-    julho,
-    agosto,
-    setembro,
-    outubro,
-    novembro,
-    dezembro
-} Meses;
-
-DiasHoje diasHoje;
-FimSemestre fimSemestre;
-
-int diasMeses(int mes)
-{
-    int dias = 0;
-    Meses meses;
-    switch (mes)
-    {
-    case janeiro:
-        dias = 30;
-        break;
-    case fevereiro:
-        dias = 28;
-        break;
-    case marco:
-        dias = 31;
-        break;
-    case abril:
-        dias = 30;
-        break;
-    case maio:
-        dias = 31;
-        break;
-    case junho:
-        dias = 30;
-        break;
-    case julho:
-        dias = 31;
-        break;
-    case agosto:
-        dias = 31;
-        break;
-    case setembro:
-        dias = 30;
-        break;
-    case outubro:
-        dias = 31;
-        break;
-    case novembro:
-        dias = 30;
-        break;
-    case dezembro:
-        dias = 31;
-        break;
-    default:
-        break;
-    }
-
-    return dias;
-}
-
-int semanasAula(DiasHoje diasHoje, FimSemestre fimSemestre)
-{
-    int semanas = 0, diaAux = diasHoje.dia;
-    FimSemestre aux;
-    int maxDiasMes;
-
-    if (diasHoje.ano < fimSemestre.ano)
-    {
-        aux = fimSemestre;
-        fimSemestre.mes = diasHoje.mes;
-    }
-    for (int mes = diasHoje.mes; mes <= fimSemestre.mes; mes++)
-    {
-        maxDiasMes = diasMeses(mes);
-
-        for (int dias = diaAux; dias <= maxDiasMes; dias++)
-        {
-            diaAux++;
-            if (diaAux % 7 == 0) // Usar módulo 7 para verificar a cada 7 dias
-            {
-                semanas++;
-                diaAux = 0;
-            }
-        }
-        if (diasHoje.mes == fimSemestre.mes && diasHoje.ano != fimSemestre.ano)
-        {
-            diasHoje.mes = 1;
-            diasHoje.ano = fimSemestre.ano;
-            fimSemestre.mes = aux.mes;
-        }
-    }
-
-    return semanas;
-}
-
-void diasDeAula(int dia, int mes, int ano, int diaFinal, int mesFinal, int anoFinal)
-{
-    int qntAulas;
-    printf("\ninforme a quantidade de aulas por semana: ");
-    scanf("%d", &qntAulas);
-    diasHoje.dia = dia;
-    diasHoje.mes = mes;
-    diasHoje.ano = ano;
-    fimSemestre.dia = diaFinal;
-    fimSemestre.mes = mesFinal;
-    fimSemestre.ano = anoFinal;
-    int semanas = semanasAula(diasHoje, fimSemestre);
-    qntAulas *= semanas;
-
-    printf("quantidade de aulas ate o fim: %d", qntAulas);
+    int diaFinal = 29, mesFinal = 10, semanas;
+    mes *= 30;
+    dias += mes;
+    mesFinal *= 30;
+    diaFinal += mesFinal;
+    semanas = (diaFinal - dias) / 7;
+    return (semanas * 2);
 }
 
 int main()
 {
     int dia, mes, ano;
-    int diaFinal, mesFinal, anoFinal;
-    printf("informe o dia de hoje (dd/mm/aaaa)\n");
-    printf("dia: ");
-    scanf("%d", &dia);
-    printf("mes: ");
-    scanf("%d", &mes);
-    printf("ano: ");
-    scanf("%d", &ano);
-
-    printf("\ninforme o termino do semestre (dd/mm/aaaa)\n");
-    printf("dia: ");
-    scanf("%d", &diaFinal);
-    printf("mes: ");
-    scanf("%d", &mesFinal);
-    printf("ano: ");
-    scanf("%d", &anoFinal);
-
-    diasDeAula(dia, mes, ano, diaFinal, mesFinal, anoFinal);
+    printf("\nInsira o dia: ");
+    scanf("%i", &dia);
+    printf("\nInsira o mes: ");
+    scanf("%i", &mes);
+    printf("\nInsira o ano: ");
+    scanf("%i", &ano);
+    printf("quantidade de aulas de laboratorio ate o final do semestre: %d\n", diasDeAula(dia, mes, ano));
     return 0;
 }
